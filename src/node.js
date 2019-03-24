7265b6fd4c0f8c6dcb7c0f7e0449c1ebd442bae8
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-03-24 18:18:49 +0300

    solve

--- src/node.js
+++ src/node.js
@@ -1,22 +1,90 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data=data;
+		this.priority=priority;
+		this.parent=null;
+		this.left=null;
+		this.right=null;
 	}
 
 	appendChild(node) {
-
+		if (this.left===null){
+			this.left=node;
+			this.left.parent = this;
+			this.left.parent.left = this.left;
+		} else if (this.right===null){
+			this.right=node;
+			this.right.parent = this;
+			this.right.parent.right = this.right;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left===node){
+			this.left.parent = null;
+			this.left=null;
+		} else if (this.right===node){
+			this.right.parent = null;
+			this.right=null;
+		} else {
+			throw new Error ("passed node is not a child of this node");
+		}
 	}
 
 	remove() {
-
+		if (this.parent === null){
+		} else {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (!this.parent) {
+			return;
+		}
+		else {
+
+			let PWithNewDP = {};
+			let granDad = this.parent.parent;
+			PWithNewDP.left = this.left;
+			PWithNewDP.right = this.right;
+
+			if (this.left) {
+				this.left.parent = this.parent;
+			}
+			if (this.right){
+				this.right.parent = this.parent;
+			}
+
+			if (this.parent.left === this) {
+				if (this.parent.right){
+					this.parent.right.parent = this;
+				}
+				this.right = this.parent.right;
+				this.left=this.parent;
+			} else if (this.parent.right === this) {
+				if (this.parent.left){
+					this.parent.left.parent = this;
+				}
+				this.left = this.parent.left;
+				this.right=this.parent;
+			}
+
+			if (this.parent.parent) {
+				if (this.parent.parent.left===this.parent) {
+					this.parent.parent.left=this;
+				} else if (this.parent.parent.right===this.parent) {
+					this.parent.parent.right=this;
+				}
+			}
+
+
+			this.parent.left = PWithNewDP.left;
+			this.parent.right = PWithNewDP.right;
+
+			this.parent.parent = this;
+			this.parent = granDad;
+		}
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,16 +1,16 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
-	}
+	constructor(maxSize=30) {
+        this.heap = new MaxHeap(maxSize);
+    }
 
 	push(data, priority) {
-
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		this.heap.pop();
 	}
 
 	size() {
@@ -18,7 +18,7 @@ class PriorityQueue {
 	}
 
 	isEmpty() {
-		
+
 	}
 }
 
